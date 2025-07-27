d0bc4708db0d461c9f2890ae871f64c87705d4c9
fento2 <ftonrate91@gmail.com>
2025-07-28 02:10:08 +0700

    object assign

--- src/lib/redux/features/accountSlice.ts
+++ src/lib/redux/features/accountSlice.ts
@@ -1,36 +1,30 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface IAccountState extends IAccount {
-    isLogin: boolean
+  isLogin: boolean;
 }
 
 const initialState: IAccountState = {
-    name: "",
-    username: "",
-    email: "",
-    profileImage: "",
-    gender: "",
-    birthDate: "",
-    isLogin: false
-}
+  name: '',
+  username: '',
+  email: '',
+  profileImage: '',
+  gender: '',
+  birthDate: '',
+  isLogin: false
+};
 
 const accountSlice = createSlice({
-    name: "account",
-    initialState,
-    reducers: {
-        login: (state, action: PayloadAction<IAccount>) => {
-            state.name = action.payload.name;
-            state.username = action.payload.username;
-            state.email = action.payload.email;
-            state.profileImage = action.payload.profileImage;
-            state.gender = action.payload.gender;
-            state.birthDate = action.payload.birthDate;
-            state.isLogin = true;
-        },
-        logout: (state) => {
-            state = state
-        }
+  name: 'account',
+  initialState,
+  reducers: {
+    login: (state, action: PayloadAction<IAccount>) => {
+      Object.assign(state, action.payload);
+    },
+    logout: () => {
+      initialState;
     }
+  }
 });
 
 export const { login, logout } = accountSlice.actions;
