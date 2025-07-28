97e48ee41379292a66cd1cba8dbe56ceae3efcd4
rzalwhy12 <rzalcorp05@gmail.com>
2025-07-29 01:36:13 +0700

    before/marge

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 'use client';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import Navbar from'@/components/Navbar' ;
+import Navbar from'@/components/navbar' ;
 import StoreProvider from './StoreProvider';
 import Footer from '@/components/Footer';
 import { usePathname } from 'next/navigation';
